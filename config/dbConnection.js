3af06cf40971443ab091fed2df5de079dca35ca2
Theogenes <41095513+thegenes93@users.noreply.github.com>
2018-11-16 23:28:42 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 var connMySQL = function () {
     return connection = mysql.createConnection({
-        host: 'mysql642.umbler.com',
-        port:'41890',
-        user: 'develoops',
-        password: 'senhaDEVE',
-        database: 'kong'
+        host: 'servidor',
+        port:'porta',
+        user: 'usuari',
+        password: 'senha',
+        database: 'nomedobanco'
     });
 }
 
